a68c08b5c38b458d0623972732acce38cb2d57e8
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-03 19:07:43 +0300

    NumOfDigits

---
+++ NumOfDigits/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+Imports System.IO
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Input an Integer Number:")
+        Dim strNum = Console.ReadLine()
+        Dim iNum As Long = 0
+        Int64.TryParse(strNum, iNum)
+        Dim nDigits = 0 : Dim iVal = Math.Abs(iNum)
+        Do While (iVal > 0)
+            iVal \= 10
+            nDigits += 1
+        Loop
+        Console.WriteLine("The number {0} has {1} digit(s)", iNum, nDigits)
+        Console.Read()
+    End Sub
+End Module
